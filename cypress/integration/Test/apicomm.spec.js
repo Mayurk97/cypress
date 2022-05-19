7447e725e26857ee31a9e02a12c10c92c0676bd6
Mayur kadam <mayurkadam215@gmail.com>
2022-05-19 12:04:15 +0530

    api2

--- cypress/integration/Test/apicomm.spec.js
+++ cypress/integration/Test/apicomm.spec.js
@@ -1,10 +1,15 @@
 
 
+
 describe('verify the comments in API',()=>{
     it('verify the GET comment',()=>{
        cy.request({
            method:'GET',
-           url:'https://jsonplaceholder.cypress.io/comments/1'
+           url:'https://jsonplaceholder.cypress.io/comments/1',
+           headers:{
+
+            "username":"mk97"
+        }
        }).then((response)=>{
            cy.log(response)
            expect(response.body.body).to.includes('laudantium')
@@ -20,6 +25,39 @@ describe('verify the comments in API',()=>{
         }).then((res)=>{
              cy.log(res)
              expect(res.status).to.eq(200)
+        })
+    })
+    it('verify post comment',()=>{
+        cy.request({
+            method:"post",
+            url:"https://reqres.in/api/users",
+            headers:{
+
+                "username":"mk97"
+            },
+            body:{
+                
+                "name": "mayur",
+                "job": "QA"
+
+            }
+        }).then((res)=>{
+            cy.log(res)
+            expect(res.body.job).to.eq('QA')
+            expect(res.status).to.eq(201)
+
+        })
+    })
+    it('verify put comment',()=>{
+        cy.request({
+           mehod:"put",
+           url:"https://reqres.in/api/users/2",
+           body:{
+            "name": "mayur",
+            "job": "Tester",
+           }
+        }).then((res)=>{
+            cy.log(res)
         })
     })
 })
---
+++ cypress/integration/Test/apicomm2.spec.js
@@ -0,0 +1,46 @@
+const { expect } = require("chai")
+
+
+describe('Api testing with alias',function(){
+    beforeEach(function(){
+        cy.request({
+            method:"GET",
+            url:"https://reqres.in/api/users?page=2"
+        }).as('pageUser')
+    })
+    it('validate the status',function(){
+        cy.get('@pageUser').its('status').should('equal',200)
+        cy.get('@pageUser').its('statusText').should('equal',"OK")
+        
+    })
+})
+
+
+describe('verify functionality for api',function(){
+    it('verify GET request to create user',function(){
+        cy.request({
+            method:"GET",
+            url:"https://reqres.in/api/users?page=2"
+        }).then(function(res){
+            cy.log(res)
+            expect(res.body).has.property('per_page',6)
+            expect(res.body).has.property('page',2)
+            expect(res.body.data).to.have.length(6)
+            res.body.data.forEach(element=>{
+                expect(element).to.have.all.keys('id','email','first_name','last_name','avatar')
+            })
+        })
+    })
+    it('verify the post request using fixture',function(){
+       cy.fixture('createUser').then(function(payload){
+           cy.request({
+               method:"POST",
+               url:"https://reqres.in/api/users",
+               body:payload
+           }).then(function(res){
+               cy.log(res)
+               expect(res.body).to.have.all.keys('id','job','name','createdAt')
+           })
+       }) 
+    })
+})
